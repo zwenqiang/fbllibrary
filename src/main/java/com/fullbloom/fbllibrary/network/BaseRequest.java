37799e97fc96448824f22051d8052903689012a1
zwq <15165868558@163.com>
2017-06-28 15:40:00 +0800

    注释

--- src/main/java/com/fullbloom/fbllibrary/network/BaseRequest.java
+++ src/main/java/com/fullbloom/fbllibrary/network/BaseRequest.java
@@ -18,11 +18,12 @@ public interface BaseRequest {
 
     }
     /**
-     * @param url
-     * @param clazz
-     * @param jsonKey
-     * @param params
-     * @param responseListener
+     * @param requestType 请求区分 利用mvp 用不到
+     * @param url 请求ur
+     * @param clazz 解析实体
+     * @param jsonKey 按需填写
+     * @param params 请求参数
+     * @param responseListener 响应回调
      *
      */
     void doRequest(int requestType, String url, Class clazz, String jsonKey, APIParams params, OnResponseListener responseListener);
